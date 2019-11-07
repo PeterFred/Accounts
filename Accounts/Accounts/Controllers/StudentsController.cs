00d023940c8ef4b599cc3fe79b916a42f0dd6234
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-07 16:28:25 +1300

    Minimal API to read from SQL Server

---
+++ Accounts/Accounts/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentDataAccess;
+
+namespace Accounts.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            using (StudentDetailsEntities entities = new StudentDetailsEntities())
+            {
+                return entities.Student.ToList();
+            }
+        }
+
+        public Student Get(int id)
+        {
+            using (StudentDetailsEntities entities = new StudentDetailsEntities())
+            {
+                return entities.Student.FirstOrDefault(e => e.student_id == id);
+            }
+        }
+    }
+}
